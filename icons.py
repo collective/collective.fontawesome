2d3ce6fb53403a38b7e12ae898720a9a5216110c
Jean-Philippe Camguilhem <jp.camguilhem@gmail.com>
2014-04-28 11:04:37 +0200

    update sample page

--- icons.py
+++ icons.py
@@ -1,8 +1,8 @@
 #This scripts read the icons.txt file to update the example page
-tpl = """<li><i class="%s"></i> <span class="icon-name">%s</span></li>"""
+tpl = """<li><i class="fa %s"></i> <span class="icon-name">%s</span></li>"""
 html = ""
-icons = open('icons.txt').read()
-for icon in icons.split('\n'):
+icons = open('icons.txt').read().splitlines()
+for icon in icons:
     html += tpl%(icon, icon) + "\n"
 
 print html
